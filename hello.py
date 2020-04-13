3b34204b7eb6be5da8e5c1b165877e11256e5ea3
twrappe <twrappe@gmail.com>
2020-04-13 12:08:03 -0700

    first commit

---
+++ hello.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+users = {
+   'users_list' :
+   [
+      {
+         'id' : 'xyz789',
+         'name' : 'Charlie',
+         'job': 'Janitor',
+      },
+      {
+         'id' : 'abc123',
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id' : 'ppp222',
+         'name': 'Mac',
+         'job': 'Professor',
+      },
+      {
+         'id' : 'yat999',
+         'name': 'Dee',
+         'job': 'Aspring actress',
+      },
+      {
+         'id' : 'zap555',
+         'name': 'Dennis',
+         'job': 'Bartender',
+      }
+   ]
+}
+@app.route('/users')
+def get_users():
+   search_username = request.args.get('name') #accessing the value of parameter 'name'
+   if search_username :
+      subdict = {'users_list' : []}
+      for user in users['users_list']:
+         if user['name'] == search_username:
+            subdict['users_list'].append(user)
+      return subdict
+   return users
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
